379c571005be96c75237fbabcdefe08a6b94d85d
André Azevedo <aazevedo525@gmail.com>
2026-02-12 21:29:38 -0300

    Add tasks index exporting task modules
    Create src/tasks/index.js to aggregate and export an array of task modules (lixo, o2, fios, download, armas, navegacao, escudo, combustivelA, combustivelB, eletricaRitmo) so the tasks can be imported as a single collection elsewhere.

---
+++ src/tasks/index.js
@@ -0,0 +1,23 @@
+const lixo = require('./lixo');
+const o2 = require('./o2');
+const fios = require('./fios');
+const download = require('./download');
+const armas = require('./armas');
+const navegacao = require('./navegacao');
+const escudo = require('./escudo');
+const combustivelA = require('./combustivelA');
+const combustivelB = require('./combustivelB');
+const eletricaRitmo = require('./eletricaRitmo');
+
+module.exports = [
+  lixo,
+  o2,
+  fios,
+  download,
+  armas,
+  navegacao,
+  escudo,
+  combustivelA,
+  combustivelB,
+  eletricaRitmo,
+];
