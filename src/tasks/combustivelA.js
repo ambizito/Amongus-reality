5f06d0d1197add1d73bd2e0b3815685c6435e484
André Azevedo <aazevedo525@gmail.com>
2026-02-12 21:28:51 -0300

    minigame combustivel

---
+++ src/tasks/combustivelA.js
@@ -0,0 +1,6 @@
+module.exports = {
+  taskId: 'COMBUSTIVEL_A',
+  label: 'Comb A',
+  qrLocationId: 'QR_COMB_A',
+  map: { x: 25, y: 45, label: 'Comb A' },
+};
---
+++ src/tasks/combustivelB.js
@@ -0,0 +1,6 @@
+module.exports = {
+  taskId: 'COMBUSTIVEL_B',
+  label: 'Comb B',
+  qrLocationId: 'QR_COMB_B',
+  map: { x: 75, y: 60, label: 'Comb B' },
+};
